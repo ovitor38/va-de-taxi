fdfd9d922a4b565a16809828be12baa3d5bb59b7
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-04-15 14:33:30 -0300

    feat(db): add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/db/migrations/{.ts,*.js}`],
+      migrationsRun: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
