52281ac9fc50b7f7b64ebe2e68dfb37f8b1654a6
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-04-15 17:24:27 -0300

    perf(app): remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 
@@ -24,7 +22,7 @@ import { UsersModule } from './modules/users/users.module';
     }),
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
