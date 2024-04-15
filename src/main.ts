0c0fe6079d8a9e60d9b9e25f0a55b91d7d2da9d2
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-04-15 17:34:03 -0300

    feat(main): add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const options = { cors: true };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, options);
   const port = process.env.PORT || 8000;
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(port);
+
   console.log(`Server is running on port: ${port} 🚀`);
 }
 bootstrap();
