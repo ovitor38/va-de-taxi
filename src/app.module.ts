c9ac0e568d1474dced3a0c313aea2c620fa7de4e
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-04-17 15:24:49 -0300

    chore(database): adjust database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as fs from 'fs';
 
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
@@ -13,11 +14,15 @@ import { MoviesModule } from './modules/movies/movies.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('certificate.pem', 'utf8'),
+      },
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
       migrations: [`${__dirname}/db/migrations/{.ts,*.js}`],
       migrationsRun: true,
