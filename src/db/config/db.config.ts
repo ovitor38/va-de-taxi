bd9b21cd9de0662380b0083ca8e240b65d435d53
Victor Oliveira <ovitor38@gmail.com>
2024-10-31 15:30:55 -0300

    chore(config): adjust no mysql db

--- src/db/config/db.config.ts
+++ src/db/config/db.config.ts
@@ -3,12 +3,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export const configDB: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 
   entities: [`${path.join(__dirname, '../../')}/**/*.entity{.js,.ts}`],
   migrations: [
